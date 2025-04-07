d2ebf78f739dba9b97db4b0205dfefa067931aaa
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2025-04-07 12:39:27 +0200

    Clase Carta

---
+++ src/Carta.java
@@ -0,0 +1,25 @@
+public class Carta {
+    private String palo;
+    private String valor;
+
+    public Carta(String palo, String valor) {
+        this.palo = palo;
+        this.valor = valor;
+    }
+
+    public int getValorNumerico(){
+        if(valor.equals("A")){
+            return 11;
+        }else if(valor.equals("K")|| valor.equals("Q") || valor.equals("J")){
+            return 10;
+        }else{
+            return Integer.parseInt(valor);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return valor + " de " + palo;
+    }
+
+}
